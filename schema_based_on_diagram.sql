c83c8e6dd1ab50d08a528156b0514bdd9a050acd
ohamed Eldeeb <agnabiieldeeeb@gmail.com>
2022-12-15 16:37:51 +0200

    Recrate the tables

--- schema_based_on_diagram.sql
+++ schema_based_on_diagram.sql
@@ -7,18 +7,41 @@ DROP TABLE IF EXISTS invoices CASCADE;
 DROP TABLE IF EXISTS invoice_items;
 
 -- Create the main tables
-CREATE TABLE medical_histories ( id INT GENERATED ALWAYS AS IDENTITY, admitted_at DATE, patient_id INT, status varchar(30), PRIMARY KEY(id));
-CREATE TABLE patients ( id INT GENERATED ALWAYS AS IDENTITY, name VARCHAR(100), date_of_birth DATE, PRIMARY KEY(id));
-CREATE TABLE invoices ( id INT GENERATED ALWAYS AS IDENTITY, total_amount FLOAT, generated_at DATE, payed_at DATE, medical_history_id INT, PRIMARY KEY (id));
-CREATE TABLE invoice_items (id INT GENERATED ALWAYS AS IDENTITY, unit_price FLOAT, quantity INT, total_price FLOAT, invoice_id INT, treatment_id INT, PRIMARY KEY (id));
-CREATE TABLE treatments (id INT GENERATED ALWAYS AS IDENTITY, type VARCHAR(60), name VARCHAR(80), PRIMARY KEY(id));
-CREATE TABLE medical_histories_treatments( treatments_id INT, medical_histories_id INT, PRIMARY KEY(treatments_id,medical_hitories_id));
+CREATE TABLE patients (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(50),
+  date_of_birth DATE,
+  PRIMARY KEY (id)
+);
 
--- ALTER TABLES TO ADD FOREIGN KEYS
-ALTER TABLE invoices ADD CONSTRAINT fk_medical_history_id FOREIGN KEY (medical_history_id) REFERENCES medical_histories(id);
-ALTER TABLE invoice_items ADD CONSTRAINT fk_invoice_id FOREIGN KEY (invoice_id) REFERENCES invoices(id);
-ALTER TABLE invoice_items ADD CONSTRAINT fk_treatment_id FOREIGN KEY (treatment_id) REFERENCES treatments(id);
-ALTER TABLE medical_histories ADD CONSTRAINT fk_patient_id FOREIGN KEY (patient_id) REFERENCES patients(id);
-ALTER TABLE medical_histories_treatments FOREIGN KEY (treatments_id) REFERENCES treatments(id);
-ALTER TABLE medical_histories_treatments FOREIGN KEY (medical_histories_id) REFERENCES medical_histories(id);
+CREATE TABLE medical_histories (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  admitted_at TIMESTAMP NOT NULL,
+  patient_id INT,
+  status VARCHAR(50),
+  CONSTRAINT fk_patient_id
+      FOREIGN KEY(patient_id)
+        REFERENCES patients(id)
+          ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
 
+CREATE TABLE invoices (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  total_amount DECIMAL NOT NULL,
+  generated_at TIMESTAMP NOT NULL,
+  payed_at TIMESTAMP NOT NULL,
+  medical_history_id INT,
+  CONSTRAINT fk_medical_history_id
+    FOREIGN KEY (medical_history_id)
+      REFERENCES medical_histories(id)
+        ON DELETE CASCADE,
+  PRIMARY KEY (id)
+);
+
+CREATE TABLE treatments (
+  id INT GENERATED ALWAYS AS IDENTITY,
+  type VARCHAR(50) NOT NULL,
+  name VARCHAR(50) NOT NULL,
+  PRIMARY KEY (id)
+);
